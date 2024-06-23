2473ab3deab0ec4143545cf8668db7a7a7ae639f
Olli-Pekka <7263059+oplehtinen@users.noreply.github.com>
2024-06-23 14:36:40 +0300

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+
+<script>
+  import "tailwindcss/tailwind.css";
+</script>
+
+<slot />
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    // get komorebic status by running the cli command komorebic status
+    const status =
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a class="btn btn-ghost text-xl">daisyUI</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li><a>Link</a></li>
+			<li>
+				<details>
+					<summary>Parent</summary>
+					<ul class="bg-base-100 rounded-t-none p-2">
+						<li><a>Link 1</a></li>
+						<li><a>Link 2</a></li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+	</div>
+</div>

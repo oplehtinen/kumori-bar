a09f8e8b3714de57739c024b6a19cd8c70a88597
Olli-Pekka <7263059+oplehtinen@users.noreply.github.com>
2024-11-27 17:16:47 +0200

    add settings bar to close (hide) settings menu, add dragging functionality

---
+++ src/routes/settings/SettingsBar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { Window } from '@tauri-apps/api/window';
+	import CloseIcon from '../Icons/CloseIcon.svelte';
+	export let currentWindow: Window;
+	onMount(() => {});
+	const closeSettingsWindow = () => {
+		if (currentWindow) currentWindow.hide();
+	};
+</script>
+
+<div
+	class="navbar bg-primary-content/95 z-50 border-b-2 border-primary/50 backdrop-blur-md sticky top-0"
+	data-tauri-drag-region
+>
+	<div class="flex-1" data-tauri-drag-region>
+		<a class="btn btn-ghost text-xl">Settings</a>
+	</div>
+	<div class="flex-none">
+		<button class="btn btn-square btn-ghost" on:click={closeSettingsWindow}>
+			<CloseIcon></CloseIcon>
+		</button>
+	</div>
+</div>
